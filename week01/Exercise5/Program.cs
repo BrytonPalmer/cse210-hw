5ae81598aa8c7ffef1f33fd50d64a9c4bab37fc7
brytonpalmer <bpalmer2@byupathway.edu>
2025-10-31 15:15:01 -0600

    exercise 5 complete

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("WELCOME TO THE PROGRAM!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string input = Console.ReadLine();
+            int favoriteNumber = int.Parse(input);
+            return favoriteNumber;
+        }
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+        static void DisplayResult(string userName, int SquareNumber)
+        {
+            Console.WriteLine($"{userName}, the square fo your favorite number is {SquareNumber}.");
+
+        }
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+        DisplayResult(userName, squaredNumber);
     }
 }
