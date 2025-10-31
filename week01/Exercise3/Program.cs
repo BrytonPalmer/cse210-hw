04926197f998f5eec7150416e681d3b79ded4d6c
brytonpalmer <bpalmer2@byupathway.edu>
2025-10-31 13:50:59 -0600

    first part of exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random RandomNumberGenerator = new Random();
+        int magicNumber = RandomNumberGenerator.Next(1, 25);
+
+        Console.WriteLine("What's your guess? ");
+        string input = Console.ReadLine();
+        int userGuess = int.Parse(input);
+
+        if (userGuess < magicNumber)
+        {
+            Console.WriteLine("Too low!");
+            Console.WriteLine("Try again!");
+        }
+        else if (userGuess > magicNumber)
+        {
+            Console.WriteLine("Too high!");
+            Console.WriteLine("Try again!");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations! You guessed the magic number!");
+        }
     }
 }
