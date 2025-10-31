3c51eb149764a8e5905cef720afc995061dc8036
brytonpalmer <bpalmer2@byupathway.edu>
2025-10-31 13:18:11 -0600

    first part of the assignment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade Percentage: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is a B.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is a C.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is a D.");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is an F.");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congradulations, you PASSED!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you FAILED. Better luck next time!");
+        }
+
     }
 }
